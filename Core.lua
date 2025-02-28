e8fb00303807dd9bef7165efb8192ee2bf497bf8
glitchstikers <glitchstikers@gmail.com>
2025-02-28 15:44:27 +0800

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [4483381587] = "https://raw.githubusercontent.com/glitchstikers/Script-/refs/heads/main/Game%20Script/a%20literal%20baseplate.lua", -- a literal baseplate
+  [4483381587] = "https://raw.githubusercontent.com/glitchstikers/Script-s/refs/heads/main/A%20literal%20Baseplate/4483381587.lua", -- a literal baseplate
 }
 
 return Games
