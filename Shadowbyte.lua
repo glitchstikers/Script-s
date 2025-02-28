670773f5aed3c34cacf464042f5a6c610f1b7f7e
glitchstikers <glitchstikers@gmail.com>
2025-02-28 15:44:51 +0800

    Update Shadowbyte.lua

--- Shadowbyte.lua
+++ Shadowbyte.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/glitchstikers/Script-/refs/heads/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/glitchstikers/Script-s/refs/heads/main/Core.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
